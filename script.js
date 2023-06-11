aaa9fa7fbd84c0d96a64b36900df89b80cb1c4bb
Ryan Delos Santos <delossantosrryan@gmail.com>
2023-06-10 20:40:17 -0700

    refactor js

--- script.js
+++ script.js
@@ -9,13 +9,7 @@ let height = document.querySelector('.height');
 let weight = document.querySelector('.weight');
 let home = document.querySelector('.home-planet');
 
-// fetch(baseUrl)
-//   .then((response) => response.json())
-//   .then((json) => {
-//     console.log((person.innerHTML = `<h1> ${json.name}</h1>`));
-//   });
-
-async function randomCharacter() {
+async function generateCharacter() {
   let randomNumber = Math.floor(Math.random() * 83) + 1;
   console.log(randomNumber);
   const personUrl = `https://swapi.dev/api/people/${randomNumber}/`;
@@ -26,19 +20,29 @@ async function randomCharacter() {
       alert('Site could not be reached');
     }
     const result = await response.json();
-    const fetchPlanet = await fetch(`${result.homeworld}`);
-    if (!fetchPlanet.ok) {
-      alert('Site could not be reached');
-    }
-    const planetData = await fetchPlanet.json();
+    const planetName = await getPlanet(result.homeworld);
     let charGender =
       result.gender.charAt(0).toUpperCase() + result.gender.slice(1);
 
     charName.innerText = result.name;
     birthYear.innerHTML = `<span class="attribute"> Birth Year:</span> ${result.birth_year}`;
     gender.innerHTML = `<span class="attribute"> Gender: </span> ${charGender}`;
-    height.innerHTML = `<span class="attribute"> Height: </span> ${result.height}`;
-    weight.innerHTML = `<span class="attribute"> Weight: </span> ${result.mass}`;
+    height.innerHTML = `<span class="attribute"> Height: </span> ${result.height}cm`;
+    weight.innerHTML = `<span class="attribute"> Mass: </span> ${result.mass}kg`;
+    return planetName;
+  } catch (error) {
+    alert('there was an error fetching the data');
+  }
+}
+
+async function getPlanet(planetUrl) {
+  try {
+    const fetchPlanet = await fetch(`${planetUrl}`);
+    if (!fetchPlanet.ok) {
+      alert('Site could not be reached');
+    }
+    const planetData = await fetchPlanet.json();
+    console.log(planetData.name);
     home.innerHTML = `<span class="attribute"> Home Planet: </span> ${planetData.name}`;
   } catch (error) {
     alert('there was an error fetching the data');
